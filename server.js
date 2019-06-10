7fb400642bd64385f13f1b3711ad6ef85974dc66
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-10 13:34:00 -0700

    added veeery basic router for note route

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
-const app = require('./lib/app');
+const app = require('./lib/app'); 
 
 const PORT = process.env.PORT || 7891;
+
+app.use('/api/v1/notes', require('./lib/routes/notes'));
 
 app.listen(PORT, () => {
   console.log(`Listening in on ${PORT}`);
